40deb6e4da2b42c8acd68f47186a7173941a1175
Konrad Sedkowski <ksedkowski@gmail.com>
2018-08-08 13:24:20 +0200

    update name function

--- js/script.js
+++ js/script.js
@@ -1,87 +1,63 @@
 'use strict';
 
-
 var output = document.getElementById('output-temp');
 
-var output_2 =document.getElementById('output-text');
+var outputText =document.getElementById('output-text');
 
 var button = document.getElementById('btn-cel');
 
-var button_2 = document.getElementById('btn-cel-2');
+var buttonFahrenheit = document.getElementById('btn-cel-2');
 
 
 
 button.addEventListener('click', function(){  
     
-  temp = window.prompt('wpisz temperature w stopniach celciusza');
+  var temp = window.prompt('wpisz temperature w stopniach celciusza');
   
-  var temp = parseFloat(temp);
+  temp = parseFloat(temp);
   
-  var tempx;
+  var tempFahrenheit = temp * 1.8 + 32;
   
-  tempx = temp * 1.8 + 32;
+  output.innerHTML = 'temperatura w stopniach celsjusza ' +temp+ ' temperatura w stopniach farenchaita '+ tempFahrenheit;
   
-  output.innerHTML = 'temperatura w stopniach celsjusza ' +temp+ ' temperatura w stopniach farenchaita '+ tempx;
-  
-   if (isNaN(temp)) {
-    window.prompt('nie podałeś liczby');
-    return 0;
-  }
-  
-  else if (temp < 0) {
-    output_2.innerHTML = 'brrr zimno';
-  }
-  
-  else if (temp === 0){
-   output_2.innerHTML = 'chyba woda zamarzła';
-  }
-  
-  else if (18 > temp && temp > 0 ) {
-    output_2.innerHTML ='nie za ciepło';
-  }
-  
-  else if ( 30 > temp && temp > 18) {
-    output_2.innerHTML ='mmm przyjemnie ciepło';
-  }
-  
-  else {
-    output_2.innerHTML ='za gorąco';
-  }
+  checkTemperature(temp);
  
 });
 
-button_2.addEventListener('click', function(){  
+buttonFahrenheit.addEventListener('click', function(){  
     
-  temp = window.prompt('wpisz temperature w stopniach celciusza');
+  var temp = window.prompt('wpisz temperature w stopniach celciusza');
   
-  var tempx;
+  var tempCelsius = (temp - 32) / 1.8;
   
-  tempx = (temp - 32) / 1.8;
+  output.innerHTML = 'temperatura w stopniach fahrenheita ' +temp+ ' temperatura w stopniach celsjusza '+ tempCelsius;  
   
-  output.innerHTML = 'temperatura w stopniach fahrenheita ' +temp+ ' temperatura w stopniach celsjusza '+ tempx;
-  
-  if (isNaN(temp)) {
+  checkTemperature(tempCelsius);
+ 
+});
+
+function checkTemperature (temp) {
+  if (isNaN(temp) || !temp) {
     window.prompt('nie podałeś liczby');
   }
   
-  else if (tempx < 0) {
-    output_2.innerHTML = 'brrr zimno';
+    else if (temp < 0) {
+    outputText.innerHTML = 'brrr zimno';
   }
   
-  else if (tempx === 0){
-   output_2.innerHTML = 'chyba woda zamarzła';
+  else if (temp === 0){
+   outputText.innerHTML = 'chyba woda zamarzła';
   }
   
-  else if (18 > tempx && tempx > 0 ) {
-    output_2.innerHTML ='nie za ciepło';
+  else if (18 >= temp && temp > 0 ) {
+    outputText.innerHTML ='nie za ciepło';
   }
   
-  else if ( 30 > tempx && tempx > 18) {
-    output_2.innerHTML ='mmm przyjemnie ciepło';
+  else if ( 30 >= temp && temp > 18) {
+    outputText.innerHTML ='mmm przyjemnie ciepło';
   }
   
   else {
-    output_2.innerHTML ='za gorąco';
+    outputText.innerHTML ='za gorąco';
   }
- 
-}); 
+}
